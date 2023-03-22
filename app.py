145eea47cae790f8cab00ccaed6ce47e1235609b
RASHEED AL-QADHI <62840251+shadowYEM@users.noreply.github.com>
2023-03-23 00:07:45 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,23 +1,19 @@
 import streamlit as st
-from transformers import pipeline, AutoTokenizer, AutoModelForSequenceClassification
+import pandas as pd
+import yfinance as yf
 
-# Load sentiment analysis model and tokenizer
-model_name = "nlptown/bert-base-multilingual-uncased-sentiment"
-tokenizer = AutoTokenizer.from_pretrained(model_name)
-model = AutoModelForSequenceClassification.from_pretrained(model_name)
+# Set page title
+st.set_page_config(page_title="Cryptocurrency Data", page_icon=":money_with_wings:")
 
-# Define function to perform sentiment analysis on input text
-def analyze_sentiment(input_text):
-    classifier = pipeline('sentiment-analysis', model=model, tokenizer=tokenizer)
-    result = classifier(input_text)
-    return result[0]
+# Set sidebar options
+st.sidebar.title("Select Cryptocurrency and Date Range")
+crypto = st.sidebar.selectbox("Select Cryptocurrency", ["BTC-USD", "ETH-USD", "LTC-USD"])
+start_date = st.sidebar.date_input("Start Date")
+end_date = st.sidebar.date_input("End Date")
 
-# Define Streamlit app
-def app():
-    st.title("Sentiment Analysis App")
-    input_text = st.text_input("Enter text to analyze:")
-    if input_text:
-        result = analyze_sentiment(input_text)
-        st.write(f"Sentiment: {result['label']}")
-        st.write(f"Score: {result['score']}")
+# Get cryptocurrency data from Yahoo Finance API
+crypto_data = yf.download(crypto, start=start_date, end=end_date)
 
+# Display cryptocurrency data in table format
+st.write(f"## {crypto} Data from {start_date} to {end_date}")
+st.write(crypto_data)
