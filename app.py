a89a289e2fa00f738b8522d0ef0b364b6cfca2e4
RASHEED AL-QADHI <62840251+shadowYEM@users.noreply.github.com>
2023-03-27 00:28:55 +0300

    Update app.py

--- app.py
+++ app.py
@@ -49,3 +49,12 @@ fig.update_layout(title=f"{tickers} Stock Prices",
 
 # Render plotly figure using streamlit
 st.plotly_chart(fig)
+
+# Add download button to download data
+def download_data():
+    csv = data.to_csv(index=False)
+    b64 = base64.b64encode(csv.encode()).decode()
+    href = f'<a href="data:file/csv;base64,{b64}" download="stock_data.csv">Download CSV file</a>'
+    return href
+
+st.markdown(download_data(), unsafe_allow_html=True)
